12c96931f9b62f0649d2229960a5c3c7a312c9e5
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-10-14 16:56:07 +0200

    Add Prettier integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
+    prettierConfig,
     {
         rules: {
             '@typescript-eslint/no-explicit-any': 'warn',
